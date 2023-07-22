0d41f95fd4c86aa1523544873d21c6c411cecec4
NeeharikaMarella03 <neeharikamarella75@gmail.com>
2023-07-22 14:41:39 +0530

    Added code for making quiz through fetching questions from api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-import Slider from './components/Slider';
-import Temperature from './components/temperature/Temperature';
+// import Slider from './components/Slider';
+// import Temperature from './components/temperature/Temperature';
+// import SignupForm from './components/signup/SignupForm';
+// import HandleInputs from './components/handlingInputs/HandleInputs'
+import Quiz from './components/quizApp/Quiz';
 
 function App() {
   return (
     <div className="App">
-      <Slider/>
+      {/* <Slider/> */}
       {/* <Temperature/> */}
+      {/* <SignupForm/> */}
+      {/* <HandleInputs /> */}
+      <Quiz />
     </div>
   );
 }
